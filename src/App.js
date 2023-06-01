0dfb68bd4fb8822de08af9b446c53f0dcfd52ae6
saakshiraut28 <saakshiraut28@gmail.com>
2023-06-01 15:31:48 +0530

    Project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+import React, { useEffect } from "react";
+import Img1 from "./images/img1.jpg";
+import Img2 from "./images/img2.jpg";
+import Dist from "./images/overlay.png";
+import hoverEffect from "hover-effect";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    new hoverEffect({
+      parent: document.querySelector(".img-section"),
+      image1: Img1,
+      image2: Img2,
+      displacementImage: Dist,
+    });
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="text-section">
+          <p className="heading">Image Distortion Effect on Hover..</p>
+          <p className="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            facilisis erat sit amet turpis consectetur, vitae varius leo
+            placerat. Proin eu maximus libero, at gravida est. Vestibulum arcu
+            enim, egestas nec gravida eget, scelerisque nec erat. Fusce volutpat
+            ipsum vitae eros maximus, in placerat arcu dignissim. Aenean vitae
+            augue leo. Fusce tincidunt porttitor aliquam. Vivamus ante erat,
+            placerat a tincidunt vitae, facilisis non velit. Duis mollis massa
+            lacus, in vehicula ipsum consequat nec.
+          </p>
+        </div>
+        <div className="img-section"></div>
+      </div>
     </div>
   );
 }
